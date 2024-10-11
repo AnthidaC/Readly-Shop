f3c2b6ab82e5021b6dd093bfd440a2ee5373fda2
Wayo03 <yosita25091994@gmail.com>
2024-10-11 16:42:55 +0700

    update
    จัดหน้าเกม

---
+++ Assets/Script/Page.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BookStore : MonoBehaviour
+{
+    public TMP_Dropdown categoryDropdown;
+    public List<GameObject> bookCategories = new List<GameObject>();
+
+    private void Start()
+    {
+        // แสดงหมวดหมู่เริ่มต้นเมื่อเริ่มเกม
+        if (bookCategories.Count > 0)
+        {
+            OnCategoryChanged(categoryDropdown.value); // แสดงหมวดหมู่เริ่มต้น
+        }
+    }
+
+    public void OnCategoryChanged(int index)
+    {
+        foreach (GameObject category in bookCategories)
+        {
+            category.SetActive(false);
+        }
+        if(index == 0)
+        {
+            bookCategories[0].SetActive(true);
+        }
+        if (index == 1)
+        {
+            bookCategories[1].SetActive(true);
+            
+        }
+    }
+}
